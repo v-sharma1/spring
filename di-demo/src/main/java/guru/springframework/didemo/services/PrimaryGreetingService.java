913808f364456658544cde20ccc128febe792ffe
Vaibhav Sharma <VAIBHAVSHARMA80@GMAIL.COM>
2018-05-20 13:21:14 +0530

    Added default profile configuration

--- di-demo/src/main/java/guru/springframework/didemo/services/PrimaryGreetingService.java
+++ di-demo/src/main/java/guru/springframework/didemo/services/PrimaryGreetingService.java
@@ -8,9 +8,16 @@ import org.springframework.stereotype.Service;
 // of GreetingService interface so that while autowiring if no 
 // qualifier is specified, this class will be injected
 
+// Use the default keyword in Profile annotation so that this bean is
+// selected when no profile value is set i.e. the config file either doesnt
+// have entry or the value is blank like shown below
+// spring.profiles.active=
+// Note, providing wrong profile value in config file like 'xyz' will
+// not trigger the default profile
+
 @Service
 @Primary
-@Profile("en")
+@Profile({"en", "default"})
 public class PrimaryGreetingService implements GreetingService {
 
 	@Override
