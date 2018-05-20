c5cf60957c627cc44f0f7dc9b520e41bd9621c2a
Vaibhav Sharma <VAIBHAVSHARMA80@GMAIL.COM>
2018-05-20 13:29:28 +0530

    Added German profile greeting

---
+++ di-demo/src/main/java/guru/springframework/didemo/services/PrimaryGermanGreetingService.java
@@ -0,0 +1,17 @@
+package guru.springframework.didemo.services;
+
+import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+@Service
+@Profile("de")
+@Primary
+public class PrimaryGermanGreetingService implements GreetingService {
+
+	@Override
+	public String sayGreeting() {
+		return "Hallo! Primärer Begrüßungsdienst";
+	}
+
+}
